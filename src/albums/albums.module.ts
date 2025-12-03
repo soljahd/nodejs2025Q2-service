3cd719df87452b1b669acb9e9308f150a92a8af7
soljah <moskuitoz@gmail.com>
2025-12-03 20:45:13 +0300

    feat: add prisma to albums

--- src/albums/albums.module.ts
+++ src/albums/albums.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AlbumsController } from './albums.controller';
 import { AlbumsService } from './albums.service';
-import { SharedModule } from '../shared/shared.module';
 
 @Module({
-  imports: [SharedModule],
   controllers: [AlbumsController],
   providers: [AlbumsService],
   exports: [AlbumsService],
